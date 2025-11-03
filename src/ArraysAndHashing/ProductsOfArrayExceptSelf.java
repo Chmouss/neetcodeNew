ae04e3a23d06f4aeb8cbb04e56b7d3740d746bbe
Chmouss <cyprien.lek@hotmail.com>
2025-11-03 01:47:18 +0100

    wip prefix suffix

--- src/ArraysAndHashing/ProductsOfArrayExceptSelf.java
+++ src/ArraysAndHashing/ProductsOfArrayExceptSelf.java
@@ -1,4 +1,27 @@
 package ArraysAndHashing;
 
+import java.util.Arrays;
+
 public class ProductsOfArrayExceptSelf {
+
+    void main(){
+        int[] nums = {1,2,4,6};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+
+//        int [] prefixValues = new int[nums.length];
+//        //int [] arrayToReturn = new int[nums.length];
+//        prefixValues[0] = nums[0];
+//        for (int i = 1; i < nums.length ; i++) {
+//            prefixValues[i] = prefixValues[i - 1] * nums[i];
+//
+//        }
+//
+//        System.out.println(Arrays.toString(prefixValues));
+
+        return null;
+    }
+
 }
